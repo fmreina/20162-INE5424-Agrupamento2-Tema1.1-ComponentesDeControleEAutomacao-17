d992de890527dcb487a516aa9d43f88da3c8be07
Eduardo Cordova <eduucordova@gmail.com>
2016-10-27 16:38:50 -0200

    Creat abstract controller

--- ine5424/include/system/config.h
+++ ine5424/include/system/config.h
@@ -11,7 +11,7 @@
 #define ARCH ia32
 #define MACH pc
 #define MMOD legacy_pc
-#define APPL pid_controller
+#define APPL pid_controller_test
 #define __mode_library__
 #define __arch_ia32__
 #define __mach_pc__
