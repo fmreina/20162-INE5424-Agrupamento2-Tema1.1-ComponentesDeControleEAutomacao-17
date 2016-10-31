7a8e79663bad17d48cba82bcb75abd6c018924e6
Eduardo Cordova <eduucordova@gmail.com>
2016-10-31 20:40:05 -0200

    Update trace and very clean

--- ine5424/include/controller.h
+++ ine5424/include/controller.h
@@ -66,28 +66,45 @@ class Controller
   public:
     // @params _kp
     float static P(float error, float prev_error, float dt, float integral, float kp) {
-    	db<Controller>(TRC) << "Controller::P(" << kp << ")" <<endl;
+    	db<Controller>(TRC) << "Controller::P(error=" << error
+    									  << ",prev_error=" << prev_error
+    									  << ",dt=" << dt
+    									  << ",integral=" << integral
+    									  << ",kp=" << kp << ")" <<endl;
 
     	CalculateP(error, kp);
     	return 0;
     }
     // @params _ki, _integral
     float static I(float error, float prev_error, float dt, float integral, float ki) {
-    	db<Controller>(TRC) << "Controller::I(" << ki << "," << integral << ")" <<endl;
+    	db<Controller>(TRC) << "Controller::I(error=" << error
+    	    									  << ",prev_error=" << prev_error
+    	    									  << ",dt=" << dt
+    	    									  << ",integral=" << integral
+    	    									  << ",ki=" << ki << ")" <<endl;
 
     	CalculateI(error, dt, ki, integral);
     	return 0;
     }
     // @params _kd
     float static D(float error, float prev_error, float dt, float integral, float kd) {
-    	db<Controller>(TRC) << "Controller::I("  << kd << ")" <<endl;
+    	db<Controller>(TRC) << "Controller::D(error=" << error
+    	    									  << ",prev_error=" << prev_error
+    	    									  << ",dt=" << dt
+    	    									  << ",integral=" << integral
+    	    									  << ",kd=" << kd << ")" <<endl;
 
     	CalculateD(error, dt, prev_error, kd);
     	return 0;
     }
     // @params _kp, _kd
     float static PD(float error, float prev_error, float dt, float integral, float kp, float kd) {
-		db<Controller>(TRC) << "Controller::PD(" << kp << "," << kd << ")" <<endl;
+		db<Controller>(TRC) << "Controller::PD(error=" << error
+    	    									  << ",prev_error=" << prev_error
+    	    									  << ",dt=" << dt
+    	    									  << ",integral=" << integral
+    	    									  << ",kp=" << kp
+    	    									  << ",kd=" << kd << ")" <<endl;
 
 		CalculateP(error, kp);
 		CalculateD(error, dt, prev_error, kd);
@@ -95,7 +112,12 @@ class Controller
 	}
     // @params _kp, _ki, integral
     float static PI(float error, float prev_error, float dt, float integral, float kp, float ki) {
-    	db<Controller>(TRC) << "Controller::PI(" << kp << ","  << ki << "," << integral << ")" <<endl;
+    	db<Controller>(TRC) << "Controller::PI(error=" << error
+    	    									  << ",prev_error=" << prev_error
+    	    									  << ",dt=" << dt
+    	    									  << ",integral=" << integral
+    	    									  << ",kp=" << kp
+    	    									  << ",ki=" << ki << ")" <<endl;
 
     	CalculateP(error, kp);
     	CalculateI(error, dt, ki, integral);
@@ -103,7 +125,13 @@ class Controller
     }
     // @params _kp, _ki, _kd, integral
     float static PID(float error, float prev_error, float dt, float integral, float kp, float ki, float kd) {
-    	db<Controller>(TRC) << "Controller::PID(" << kp << "," << ki << "," << kd << "," << integral << ")" <<endl;
+    	db<Controller>(TRC) << "Controller::PID(error=" << error
+    	    									  << ",prev_error=" << prev_error
+    	    									  << ",dt=" << dt
+    	    									  << ",integral=" << integral
+    	    									  << ",kp=" << kp
+    	    									  << ",ki=" << ki
+    	    									  << ",kd=" << kd << ")" <<endl;
 
     	CalculateP(error, kp);
     	CalculateI(error, dt, ki, integral);
@@ -112,7 +140,7 @@ class Controller
     }
   protected:
     float static CalculateP(float error, float kp) {
-    	db<Controller>(TRC) << "CalculateP(" << error << ", " << kp << ")" << endl;
+    	db<Controller>(TRC) << "CalculateP(error=" << error << ",kp=" << kp << ")" << endl;
 
     	// proportional result
     	float pOut = kp * error;
@@ -120,14 +148,14 @@ class Controller
         return pOut;
     }
     float static CalculateI(float error, float dt, float ki, float integral) {
-        db<Controller>(TRC) << "CalculateI(" << error << ", " << ki << ", " << integral << endl;
+        db<Controller>(TRC) << "CalculateI(error=" << error << ",ki=" << ki << ",integral=" << integral << endl;
 
         float iOut = ki * integral;
 
         return iOut;
     }
     float static CalculateD(float error, float dt, float prev_error, float kd) {
-    	db<Controller>(TRC) << "CalculateD(" << error << ", " << prev_error << ", " << kd << ")"<< endl;
+    	db<Controller>(TRC) << "CalculateD(error=" << error << ",prev_error=" << prev_error << ",kd=" << kd << ")"<< endl;
 
         // Derivative term
         float derivative = (error - prev_error) / dt;
