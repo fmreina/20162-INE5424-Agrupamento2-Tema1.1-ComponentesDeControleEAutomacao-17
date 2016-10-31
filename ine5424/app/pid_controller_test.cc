f64a3eec70445fbd250b6c566fedd512524de368
Eduardo Cordova <eduucordova@gmail.com>
2016-10-31 20:09:21 -0200

    All controller working :)

--- ine5424/app/pid_controller_test.cc
+++ ine5424/app/pid_controller_test.cc
@@ -11,45 +11,40 @@ int main()
 {
     cout << ".:Proportional Integrative Derivative Controller Test:." << endl;
 
-    float max = 70;
-    float min = -70;
-    float dOutput, iOutput, pOutput, output;
+    float max = 2;
+    float min = -1;
 
     float kd = 1.65;
     float ki = 1.65;
-    float kp = 1.65;
+    float kp = 1.16;
     float integral = 0.55;
     float dt = 0.5;
-    float pv = 20;
-    float setpoint = 50;
+    float setpoint = 1;
     cout << "Setting values "
          << "\nkp=" << kp
          << "\nki=" << ki
          << "\nkd=" << kd
          << "\ndt=" << dt
-         << "\npv=" << pv
          << "\nst=" << setpoint
     << endl;
 
 	cout << "Initializing P controller!" << endl;
-	Controller* pctrl = new Controller(10, 10, dt, &Controller::P, kp);
+	Controller* pctrl = new Controller(10, 10, max, min, setpoint, dt, &Controller::P, kp);
 
 	cout << "Initializing I controller!" << endl;
-	Controller* ictrl = new Controller(10, 10, dt, &Controller::I, ki, integral);
+	Controller* ictrl = new Controller(10, 10, max, min, setpoint, dt, &Controller::I, ki);
 
 	cout << "Initializing D controller!" << endl;
-	Controller* dctrl = new Controller(10, 10, dt, &Controller::D, kd);
+	Controller* dctrl = new Controller(10, 10, max, min, setpoint, dt, &Controller::D, kd);
 
 	cout << "Initializing PD controller!" << endl;
-	Controller* pdctrl = new Controller(10, 10, dt, &Controller::PD, kp, kd);
+	Controller* pdctrl = new Controller(10, 10, max, min, setpoint, dt, &Controller::PD, kp, kd);
 
 	cout << "Initializing PI controller!" << endl;
-	Controller* pictrl = new Controller(10, 10, dt, &Controller::PI, kp, ki, integral);
+	Controller* pictrl = new Controller(10, 10, max, min, setpoint, dt, &Controller::PI, kp, ki);
 
 	cout << "Initializing PID controller!" << endl;
-	Controller* pidctrl = new Controller(10, 10, dt, &Controller::PID, kp, ki, kd, integral);
-
-    cout << "output = " << output << endl;
+	Controller* pidctrl = new Controller(10, 10, max, min, setpoint, dt, &Controller::PID, kp, ki, kd);
 
     cout << "The end!" << endl;
 
